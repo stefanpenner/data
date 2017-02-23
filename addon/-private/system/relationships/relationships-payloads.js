71875ba493dc2fb1e30d3ae1fc7a08d20ecf0744
David J. Hamilton <dhamilton@linkedin.com>
2017-02-22 18:15:13 -0800

    add comments

--- addon/-private/system/relationships/relationships-payloads.js
+++ addon/-private/system/relationships/relationships-payloads.js
@@ -1,17 +1,112 @@
 import Ember from 'ember';
 let get = Ember.get;
 
+/**
+  Manages relationship payloads for a given store.  Acts as a single source of
+  truth (of payloads) for both sides of a relationship so they can agree on the
+  most up-to-date payload received without needing too much eager processing
+  when those payloads are pushed into the store.
+
+  @example
+
+    let relationshipsPayloads = new RelationshipsPayloads(store);
+
+    const User = DS.Model.extend({
+      hobbies: DS.hasMany('hobby')
+    });
+
+    const Hobby = DS.Model.extend({
+      user: DS.belongsTo('user')
+    });
+
+    let userPayload = {
+      data: {
+        id: 1,
+        type: 'user',
+        relationships: {
+          hobbies: {
+            data: [{
+              id: 2,
+              type: 'hobby'
+            }]
+          }
+        }
+      },
+    };
+    relationshipsPayloads.push('user', 1, userPayload.data.relationships);
+
+    relationshipsPayloads.get('hobby', 2, 'user') === {
+      {
+        payload: {
+          data: {
+            id: 1,
+            type: 'user'
+          }
+        },
+        // tells us this payload was populated from the inverse side; so if
+        // we're a many side of a relationship we are not fully loaded
+        inverse: true,
+      }
+    }
+
+  @class RelationshipsPayloads
+*/
 export default class RelationshipsPayloads {
   constructor(store) {
     this._store = store;
     this._map = {};
   }
 
+  /**
+    Find the payload for the given relationship of the given model.
+
+    Returns a payload entry which will contain the payload for the given
+    relationship and a flag to denote whether this payload was directly pushed
+    or computed from an inverse
+
+    @example
+
+      relationshipsPayloads.get('hobby', 2, 'user') === {
+        {
+          payload: {
+            data: {
+              id: 1,
+              type: 'user'
+            }
+          },
+          // tells us this payload was populated from the inverse side; so if
+          // we're a many side of a relationship we are not fully loaded
+          inverse: true,
+        }
+      }
+
+  */
   get(modelName, id, relationshipName) {
     let relationshipPayloads = this._getRelationshipPayloads(modelName, relationshipName);
     return relationshipPayloads && relationshipPayloads.get(modelName, id, relationshipName);
   }
 
+  /**
+    Push a model's relationships payload into this cache.
+
+    @example
+
+      let userPayload = {
+        data: {
+          id: 1,
+          type: 'user',
+          relationships: {
+            hobbies: {
+              data: [{
+                id: 2,
+                type: 'hobby'
+              }]
+            }
+          }
+        },
+      };
+      relationshipsPayloads.push('user', 1, userPayload.data.relationships);
+  */
   push(modelName, id, relationshipsData) {
     if (!relationshipsData) { return; }
 
@@ -23,6 +118,24 @@ export default class RelationshipsPayloads {
     }
   }
 
+  /**
+    Find the RelationshipPayloads object for the given relationship.  The same
+    RelationshipPayloads object is returned for either side of a relationship.
+
+    @example
+
+      const User = DS.Model.extend({
+        hobbies: DS.hasMany('hobby')
+      });
+
+      const Hobby = DS.Model.extend({
+        user: DS.belongsTo('user')
+      });
+
+      relationshipPayloads.get('user', 'hobbies') === relationshipPayloads.get('hobby', 'user');
+
+    @private
+  */
   _getRelationshipPayloads(modelName, relationshipName) {
     // TODO: lightschema this
     let modelClass = this._store._modelFor(modelName);
@@ -44,6 +157,11 @@ export default class RelationshipsPayloads {
     return this._map[key];
   }
 
+  /**
+    Find the invese of a given relationship.
+
+    @return {[String, String]} A tuple of the inverse model and inverse relationship names
+  */
   _inverseFor(modelName, relationshipName) {
     // TODO: lightschema this
     let modelClass = this._store._modelFor(modelName);
@@ -64,6 +182,52 @@ export default class RelationshipsPayloads {
   }
 }
 
+
+/**
+  Manages the payloads for both sides of a single relationship, across all model
+  instances.
+
+  The "key" for one side of a relationship is given by
+  `${modelName}:${relationshipName}`.
+
+  For example, with
+
+    const User = DS.Model.extend({
+      hobbies: DS.hasMany('hobby')
+    });
+
+    const Hobby = DS.Model.extend({
+      user: DS.belongsTo('user')
+    });
+
+    let key1 = "user:hobbies";
+    let key2 = "hobby:user";
+    let relationshipPayloads = new RelationshipPayloads(key1, key2);
+
+    let userPayload = {
+      data: {
+        id: 1,
+        type: 'user',
+        relationships: {
+          hobbies: {
+            data: [{
+              id: 2,
+              type: 'hobby',
+            }]
+          }
+        }
+      }
+    };
+
+    // here we expect the payload of the individual relationship
+    relationshipPayloads.push('user', 1, 'hobbies', userPayload.data.relationships.hobbies);
+
+    relationshipPayloads.get('user', 1, 'hobbies');
+    relationshipPayloads.get('hobby', 2, 'user');
+
+  @class RelationshipPayloads
+  @private
+*/
 class RelationshipPayloads {
   constructor(key1, key2) {
     this._lhsKey = key1;
